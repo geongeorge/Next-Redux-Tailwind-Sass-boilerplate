a7490187d28abf4e296656c177e6643392468f63
Geon George <geongeorge98@gmail.com>
2020-08-13 00:36:23 +0530

    Updates readme

--- components/intro.js
+++ components/intro.js
@@ -3,7 +3,10 @@
 export default function Intro() {
   return (
     <div className="intro">
-      Hello from <a href="#">tailwind-next-sass</a>
+      Hello from{" "}
+      <a href="https://github.com/geongeorge/Next-Redux-Tailwind-Sass-boilerplate">
+        next-redux-tailwind-sass
+      </a>
     </div>
   );
 }
